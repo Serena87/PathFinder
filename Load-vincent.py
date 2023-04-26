52f225a59ad32c1b049e6f8308d028f64c1db52f
bohmaniac <vincent.isegranbohman@gmail.com>
2023-04-26 10:26:45 +0200

    Notebook

--- Load-vincent.py
+++ Load-vincent.py
@@ -5,6 +5,7 @@
 import pandas as pd
 # read the file called 2022.json that is in the same directory and call it jobtech_dataset
 jobtech_dataset = pd.read_csv('jobtech_dataset2022.csv')
-df = jobtech_dataset[['id', 'description']]
+#
+df = jobtech_dataset[['id', 'description', 'working_hours_type']]
 # #show the variables names (columns) in the dataset
 print(df)
