69fbaa1ebd54fa16cafae9a640113f713ae73c5c
AugKra <131650115+AugKra@users.noreply.github.com>
2023-05-02 11:07:48 +0200

    Uppdaterad frontend

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-#from flask import Flask, render_template
-
-#app = Flask(__name__)
-
-#@app.route("/pathfinder")
-#def index():
-    #return render_template('template/index.html')
-
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
