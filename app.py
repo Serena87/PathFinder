f74b4b231d59ce99d06c756ac60cb750fade02ae
AugKra <131650115+AugKra@users.noreply.github.com>
2023-05-03 10:18:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-
+    
 from flask import Flask, render_template
 from backend import get_words
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/pathfinder')
 def button_cluster():
     # Define the list of keywords to the buttons:
-    button_keywords = ["Musik", "Fotografera", "Design", "Social"]
+    button_keywords = get_words()
     #button_keywords = get_words()
     button_templates = ["blue_button_template", "green_button_template", "orange_button_template"]
 
@@ -18,23 +18,24 @@ def button_cluster():
     with open('templates/green_button_template.html', 'r') as fgreen:
         green_button_template = fgreen.read()
     with open('templates/orange_button_template.html', 'r') as forange:
-    # Read the CSS style for the buttons and the cluster from file:    
         orange_button_template = forange.read()
     with open('templates/style.css', 'r') as stylesheet:
         css_read = stylesheet.read()
 
     # Generate the HTML code for the button cluster
-    x = 0
     html = "<!DOCTYPE html><html><head>"
     html += css_read
     html += "</head><body><div class='button-cluster'>"
-    for name in button_keywords:
-        button_html = green_button_template.format(name=name)
-        #button = button_templates(x)
-        #button_html = button 
-        #button_html += ".format(name=name)"
+    for i, name in enumerate(button_keywords):
+        template_index = i % len(button_templates)
+        template = button_templates[template_index]
+        if template == "blue_button_template":
+            button_html = blue_button_template.format(name=name)
+        elif template == "green_button_template":
+            button_html = green_button_template.format(name=name)
+        else:
+            button_html = orange_button_template.format(name=name)
         html += button_html
-        x += 1
     html += "</div></body></html>"
 
     # Render the HTML code as a response
@@ -42,4 +43,3 @@ def button_cluster():
 
 if __name__ == '__main__':
     app.run()
-    
