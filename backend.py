7dbdf84e4fb4a74a490ab7560514c96f716271bf
bohmaniac <vincent.isegranbohman@gmail.com>
2023-05-03 16:05:15 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,4 +1,8 @@
-
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import Pipeline
 #Skickar ord att fylla bubblorna med till frontend
 lista = ['Noggran', 'Kreativ', 'Djur', 'Körkort', 'Positiv', 'Människor', 'Mat', 'Hållbarhet', 'Vårdande', 'Social', 'Försäljning']
 
@@ -52,4 +56,61 @@ def get_occupation(word1, word2, word3):
     return top_occupations
 
 
-print(get_occupation('kund', 'vård', 'hjälp'))
+print(get_occupation('utåtriktad', 'körkort', 'människor'))
+
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def get_occupation2(word1, word2, word3):
+    # Load occupation data
+    df = pd.read_csv('clean_occup.csv')
+
+    # Preprocess occupation descriptions
+    tfidf = TfidfVectorizer(stop_words='english')
+    description_vectors = tfidf.fit_transform(df['description'].fillna(''))
+
+    # Create input vector
+    input_vector = tfidf.transform([' '.join([word1, word2, word3])])
+
+    # Compute cosine similarity between input vector and occupation description vectors
+    similarity_scores = cosine_similarity(input_vector, description_vectors)
+
+    # Rank occupations by similarity score and return top 5
+    top_occupations = np.argsort(similarity_scores, axis=1)[:, -5:].squeeze()[::-1]
+    return df.iloc[top_occupations]['occupation'].tolist()
+
+print(get_occupation2('utåtriktad', 'körkort', 'människor'))
+
+import pandas as pd
+import numpy as np
+import gensim.downloader as api
+from gensim.models import Word2Vec
+from gensim.similarities import SoftCosineSimilarity
+from sklearn.preprocessing import normalize
+
+def get_occupation3(word1, word2, word3):
+    # Load occupation data
+    df = pd.read_csv('clean_occup.csv')
+
+    # Preprocess occupation descriptions
+    model = api.load('glove-wiki-gigaword-300')
+    w2v = dict(zip(model.wv.index2word, model.wv.vectors))
+    doc_vectors = [np.mean([w2v[w] for w in doc.split() if w in w2v], axis=0) for doc in df['description'].fillna('')]
+    norm_vectors = normalize(doc_vectors)
+
+    # Create input vector
+    input_vector = np.mean([w2v[w] for w in [word1, word2, word3] if w in w2v], axis=0).reshape(1, -1)
+    norm_input = normalize(input_vector)
+
+    # Compute Soft Cosine Similarity between input vector and occupation description vectors
+    similarity_matrix = SoftCosineSimilarity(norm_vectors)
+    similarity_scores = similarity_matrix[norm_input][0]
+
+    # Rank occupations by similarity score and return top 5
+    top_occupations = np.argsort(similarity_scores)[-5:][::-1]
+    return df.iloc[top_occupations]['occupation'].tolist()
+
+
+print(get_occupation3('utåtriktad', 'körkort', 'människor'))
