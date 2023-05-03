2cb5278e0e784fa3a3a6096a275192ab48d439b0
Serena87 <gusstinst@student.gu.se>
2023-05-03 09:45:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 @app.route('/pathfinder')
 def button_cluster():
     # Define the list of keywords to the buttons:
-    button_keywords = ["Musik", "Fotografera", "Design", "Social"]
-    #button_type = ["circle-blue", "circle-green", "circle-orange"]
+    button_keywords = ["Musik", "Fotografera", "Design"]
+    button_templates = ["blue_button_template", "green_button_template", "orange_button_template"]
 
     # Read the button templates from the html files:
     with open('templates/blue_button_template.html', 'r') as fblue:
@@ -20,12 +20,17 @@ def button_cluster():
         css_read = stylesheet.read()
 
     # Generate the HTML code for the button cluster
+    x = 0
     html = "<!DOCTYPE html><html><head>"
     html += css_read
     html += "</head><body><div class='button-cluster'>"
     for name in button_keywords:
-        button_html = blue_button_template.format(name=name)
+        button_html = green_button_template.format(name=name)
+        #button = button_templates(x)
+        #button_html = button 
+        #button_html += ".format(name=name)"
         html += button_html
+        x += 1
     html += "</div></body></html>"
 
     # Render the HTML code as a response
