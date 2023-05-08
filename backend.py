0359e8e54bb73c07db42346a919da8c1932c4d05
bohmaniac <vincent.isegranbohman@gmail.com>
2023-05-08 10:27:38 +0200

    Update backend.py

--- backend.py
+++ backend.py
@@ -31,19 +31,24 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def get_occupation2(word1, word2, word3, word4, word5):
     # Load occupation data
+    print('Reading dataset...')
     df = pd.read_csv('clean_occup.csv')
+    print('dataset read!')
 
     # Preprocess occupation descriptions. Puts words into vectors for each  
     tfidf = TfidfVectorizer()
     description_vectors = tfidf.fit_transform(df['description'].fillna(''))
+    print('preprocessed occupation descriptions! Vectors cretead')
 
     # Create input vector by transforming a string containing three words (word1, word2, and word3) 
     # using the same TfidfVectorizer instance. The resulting vector is stored in a variable called "input_vector".
     input_vector = tfidf.transform([' '.join([word1, word2, word3, word4, word5])])
+    print('Input vector created')
 
     # Compute cosine similarity (similarity between two vectors) between input vector and occupation description vectors
     similarity_scores = cosine_similarity(input_vector, description_vectors)
 
+    print('Similarity between input vector, description vector compared')
     # Rank occupations by similarity score and return top 5
     top_occupations = np.argsort(similarity_scores, axis=1)[:, -5:].squeeze()[::-1]
     return df.iloc[top_occupations]['occupation'].tolist()
@@ -52,75 +57,62 @@ def get_occupation2(word1, word2, word3, word4, word5):
 print(get_occupation2('ledarskap', 'utveckling', 'utbildning', 'forskning', ''))
 
 
-## Experiment
 
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
-from collections import Counter
 import random
 
-def extract_common_words():
-    # load the dataset
-    df = pd.read_csv('clean_occup.csv')
 
-    # tokenize the descriptions
+
+## Ny extract_common_words
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+import random
+
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def extract_common_words(dataset_file):
+    # Load the dataset
+    df = pd.read_csv(dataset_file)
+
+    # Tokenize the descriptions
     tokenizer = nltk.RegexpTokenizer(r'\w+')
     df['tokens'] = df['description'].apply(lambda x: tokenizer.tokenize(x.lower()))
 
-    # remove stop words
+    # Remove stop words
     stop_words = stopwords.words('swedish')
     df['tokens'] = df['tokens'].apply(lambda x: [word for word in x if word not in stop_words])
 
-    # count the word frequency
-    word_counts = Counter([word for words in df['tokens'] for word in words])
+    # Join tokenized words into a single string
+    df['tokens'] = df['tokens'].apply(lambda x: ' '.join(x))
 
-    # extract 50 random but common words
-    common_words = [word[0] for word in word_counts.most_common()]
-    random.shuffle(common_words)
-    common_words = common_words[:50]
+    # Create a TF-IDF vectorizer
+    vectorizer = TfidfVectorizer()
+
+    # Fit the vectorizer on the tokenized descriptions
+    X = vectorizer.fit_transform(df['tokens'])
+
+    # Get the feature names (words) from the vectorizer
+    feature_names = vectorizer.get_feature_names_out()
+
+    # Convert feature_names to a list
+    feature_names = list(feature_names)
+
+    # Extract 50 random but common words
+    common_words = random.sample(feature_names, 50)
 
     return common_words
 
-print(extract_common_words)
+# Example usage
+common_words = extract_common_words('clean_occup.csv')
+print(common_words)
 
 
-## GAMMAL GET_OCCUPATION ALGORITM - INTE ALLS BRA
-#Takes three words and returns 5 matching occupations
 
-def get_occupation(word1, word2, word3, word4, word5):
 
-    import pandas as pd
-    from sklearn.feature_extraction.text import TfidfVectorizer
-    from sklearn.metrics.pairwise import cosine_similarity
 
-    # load your dataset into a Pandas DataFrame
-    df = pd.read_csv('clean_occup.csv')
-
-    # create a TfidfVectorizer object to convert the text descriptions into a numerical matrix
-    tfidf = TfidfVectorizer()
-
-    # fit the TfidfVectorizer object to the text descriptions of the occupations
-    tfidf_matrix = tfidf.fit_transform(df['description'])
-
-    # create a list of the input words
-    input_words = [word1, word2, word3, word4, word5]
-
-    # convert the input words into a numerical matrix using the TfidfVectorizer object
-    input_matrix = tfidf.transform(input_words)
-
-    # calculate the cosine similarity between the input matrix and the tfidf matrix for all occupations
-    cosine_similarities = cosine_similarity(input_matrix, tfidf_matrix)
-
-    # get the top 5 occupations with the highest similarity scores
-    top_indexes = cosine_similarities.argsort()[0][-5:][::-1]
-
-    # create a list of the top 5 occupations with the highest similarity scores
-    top_occupations = []
-    for index in top_indexes:
-        top_occupations.append(df.iloc[index]['occupation'])
-
-    # print the top 5 occupations with the highest similarity scores
-    #print("The input words match with the following occupations:")
-    #print(top_occupations)
-    return top_occupations
