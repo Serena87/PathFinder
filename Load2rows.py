7702b9e7d32b1298908612e69ce261a1284df55b
beatsie13 <99176748+beatsie13@users.noreply.github.com>
2023-05-08 16:25:59 +0200

    nu fungerande load

--- Load2rows.py
+++ Load2rows.py
@@ -23,14 +23,16 @@ print(jobtech_dataset.columns)
 #show the first 3 rows (job postings) in the dataset
 print(jobtech_dataset.head(3))
 
-jobtech_dataset.info()
+#jobtech_dataset.info()
 
 # crop dataset to only wanted columns
 dataframe = jobtech_dataset[['description', 'occupation']] 
 
-dataframe.info()
+#dataframe.info()
 
 df = pd.DataFrame(columns=dataframe.columns)
+
+print('detecting swedish ads..')
 
 # iterate over the rows of the original dataframe
 for index, row in dataframe.iterrows():
@@ -39,28 +41,52 @@ for index, row in dataframe.iterrows():
         # if the language is Swedish, append the row to the new dataframe
         df = df.append(row, ignore_index=True)
 
-
-
-# Välj kolumnen som du vill ändra och använd str-metoden slice för att ta bort de första 10 tecknen
+# cleaning the column description
+print('cleaning description column..')
 df['description'] = df['description'].str.slice(start=10)
 df['description'] = df['description'].str.replace('\\', 'KOWABUNGA')
 df['description'] = df['description'].str.replace('KOWABUNGAn', ' ')
+df['description'] = df['description'].str.replace('KOWABUNGAu202f', ' ')
+df['description'] = df['description'].str.replace('KOWABUNGAt', ' ')
+df['description'] = df['description'].str.replace('KOWABUNGAxad', ' ')
+df['description'] = df['description'].str.replace('KOWABUNGAx95', ' ')
+df['description'] = df['description'].str.replace('KOWABUNGAxa0', ' ')
+df['description'] = df['description'].str.replace('KOWABUNGA', ' ')
 df['description'] = df['description'].replace(replace_dict, regex=True)
 
 #Remove stop words from description column
+print('removing stopwords..')
 stop_words = set(stopwords.words('swedish'))
 df['description'] = df['description'].apply(lambda x: ' '.join([word for word in word_tokenize(x) if word.lower() not in stop_words]))
 
 
 # Clean Occupation column
+print('cleaning occupation column..')
 df['occupation'] = df['occupation'].str.slice(start=40)
 df['occupation'] = df['occupation'].str.split("'", 2).str[1]
 
-
+'''
 print(df.columns)
 #show the first 3 rows (job postings) in the dataset
 print(df.head(3))
-df.info()
+df.info()'''
+
+# loop through each unique occupation and find the corresponding description(s)
+print('finding unique occupations..')
+occupations = df['occupation'].unique()
+occupation_descriptions = []
+for occupation in occupations:
+    description = ""
+    for index, row in df.iterrows():
+        if row['occupation'] == occupation:
+            description += row['description'] + " "
+    occupation_descriptions.append(description.strip())
+
+# create a new dataframe with the occupations and descriptions
+new_df = pd.DataFrame({'occupation': occupations, 'description': occupation_descriptions})
+
+# print the new dataframe
+print(new_df)
 
 # save the new dataframe to a CSV file
-df.to_csv('dataset.csv', index=False)
+new_df.to_csv('dataset2022.csv', index=False)
