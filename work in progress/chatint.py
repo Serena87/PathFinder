a3852a22623bc010900353e2ea23137faa949018
beatsie13 <99176748+beatsie13@users.noreply.github.com>
2023-05-19 11:12:07 +0200

    Uppdatead promt 3 bra saker

--- work in progress/chatint.py
+++ work in progress/chatint.py
@@ -1,14 +1,14 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 print('öppnar')
 completion = openai.ChatCompletion.create(
     model = "gpt-3.5-turbo",
     temperature = 0.2,
     max_tokens = 1000,
     messages = [
-        {"role": "system", "content": "Du är en yrkesvägledare, beskriv yrket och förutspå framtiden för yrket."},
-        {"role": "user", "content": "frisör."}])
+        {"role": "system", "content": "beskriv och förutspå framtiden för yrket, avsluta med 3 bra saker med yrket."},
+        {"role": "user", "content": "maskinoperatör, fisk."}])
 print('kör')
 print(completion.choices[0].message.content)
 print('klart')
