451c99ebbc3f828ec5ade29ffba0cb42de0f9864
Serena87 <gusstinst@student.gu.se>
2023-05-23 09:30:06 +0200

    cleaned a little bit

--- list_work.py
+++ list_work.py
@@ -4,12 +4,6 @@ import random
 nyckelord_tasks = get_egenskaper()
 nyckelord_attributes = get_arbetsuppgifter()
 
-#print('Nyckelord arbetsuppgifter: ' + str(len(nyckelord_tasks)) + ' ord')
-#print(nyckelord_tasks )
-#print()
-#print('Nyckelord egenskaper: ' + str(len(nyckelord_attributes)) + ' ord')
-#print(nyckelord_attributes)
-
 def list_mixer(list1, list2):
     list_mixed = []
     for x in range (len(list1)):
@@ -18,9 +12,7 @@ def list_mixer(list1, list2):
         list_mixed.append(list2[y])
     return list_mixed
 
-#print()
 nyckelord_mixed = list_mixer(nyckelord_tasks, nyckelord_attributes)
 random.shuffle(nyckelord_mixed)
-#print('Nyckelord blandat: ' + str(len(nyckelord_mixed)) + ' ord')
-#print( nyckelord_mixed)
+
 
